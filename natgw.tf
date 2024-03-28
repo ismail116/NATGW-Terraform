6f80ca654334e141d12d60c1aa4c0cd53b57a315
Ismail Mohamed Helal <esmailhelal78@gmail.com>
2024-03-28 14:46:48 +0200

    init commit

--- natgw.tf
+++ natgw.tf
@@ -10,5 +10,6 @@ resource "aws_eip" "nat_eip" {
 resource "aws_nat_gateway" "nat_gateway" {
   allocation_id = aws_eip.nat_eip.id
   subnet_id     = aws_subnet.subnetn2.id
+  depends_on = [aws_internet_gateway.example]
 }
 
