301930d450c6ac59f09a1b968d4b580f9ae1a9d7
Ismail Mohamed Helal <esmailhelal78@gmail.com>
2024-03-28 15:10:19 +0200

    init commit

--- ec2.tf
+++ ec2.tf
@@ -4,7 +4,7 @@ resource "aws_instance" "instancen" {
   instance_type   = var.ec2_type
   subnet_id       = aws_subnet.subnetn.id
   security_groups = [aws_security_group.instance_sg.name] # Associate the instance with the security group
-  user_data       = file("script.sh")
+  user_data       =  "${file("script.sh")}"
   tags = {
     Name = "task-instance"
   }
--- natgw.tf
+++ natgw.tf
@@ -3,13 +3,12 @@ resource "aws_eip" "nat_eip" {
   #vpc = true
   domain = "vpc"
   #vpc_id = aws_vpc.vpctask2.id
-
 }
 
 # Create NAT Gateway & associated with public subnet
 resource "aws_nat_gateway" "nat_gateway" {
   allocation_id = aws_eip.nat_eip.id
   subnet_id     = aws_subnet.subnetn2.id
-  depends_on = [aws_internet_gateway.igwn]
+  depends_on    = [aws_internet_gateway.igwn]
 }
 
